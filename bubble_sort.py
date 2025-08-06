184ae34cad18881984373599d421de5aa3e11c29
Venkataramanan Venkateswaran <venk@Venkataramanans-MacBook-Air.local>
2025-08-05 18:38:27 -0700

    Add. Bubble Sort

---
+++ bubble_sort.py
@@ -0,0 +1,22 @@
+def bubble_sort(nums):
+    n = len(nums)
+    for i in range(n - 1, 0, -1):
+        for j in range(i):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+    
+    return nums
+
+
+my_list = []
+num_elements = int(input("How many elements will you enter? "))
+for _ in range(num_elements):
+    element = input("Enter an element: ")
+    my_list.append(element)
+
+print("Unsorted list")
+print(my_list)
+
+my_list = bubble_sort(my_list)
+print("Sorted list")
+print(my_list)
